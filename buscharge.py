504a6e19f6a410e4459f0e77cd0d75a43ccf3005
Mathiarasu R S <124028262+Mathiarasu05@users.noreply.github.com>
2023-06-19 20:43:44 +0530

    Add files via upload

---
+++ buscharge.py
@@ -0,0 +1,9 @@
+print("welcome to ABC omini bus company")
+year=int(input("enter your birth year: "))
+age=2023-year
+print(age)
+if age>=60:
+    fare=1020-(4/2)*102
+    print("the traveling charge is: ",fare)
+else:
+    print("the traveling charge is: 1020")
