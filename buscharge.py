fc5f608b768faf20e2a905b9343d0cb03794db22
Mathiarasu R S <124028262+Mathiarasu05@users.noreply.github.com>
2023-06-22 05:17:18 +0530

    Update buscharge.py

--- buscharge.py
+++ buscharge.py
@@ -1,3 +1,6 @@
+#ACB, the Omni Bus Company, has decided to give a 20% traveling fare concession for senior citizens; the actual fare will be Rs 1020.
+#The upper age limit for senior citizens is above 60. 
+#For verification, they will ask for the date of birth (take the year alone), and if satisfied, they will be given a concession and will be displayed the final traveling charge; if not, the original fare will be displayed. 
 print("welcome to ABC omini bus company")
 year=int(input("enter your birth year: "))
 age=2023-year
