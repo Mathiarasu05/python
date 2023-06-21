328b03e7d6c632da255ba5a06f7e3a469d3e3f9a
Mathiarasu R S <124028262+Mathiarasu05@users.noreply.github.com>
2023-06-22 05:12:34 +0530

    Update clubblood.py

--- clubblood.py
+++ clubblood.py
@@ -1,3 +1,6 @@
+#A Rotary Club has decided to conduct a blood donation camp.
+#The eligibility criteria for the blood donation camp will be that the person should be above 18 and his or her weight should be above 40. 
+#The volunteers will enter the name, age, and weight of the person, and then it will display whether they are eligible or not.
 print("welcome to blood donation camp")
 name=str(input("enter your name: "))
 age=int(input("enter your age: "))
