445ad1a712abab5c07df08482980d9cb5204f4d7
Mathiarasu R S <124028262+Mathiarasu05@users.noreply.github.com>
2023-06-21 09:32:22 +0530

    Add files via upload

---
+++ theorem.py
@@ -0,0 +1,11 @@
+n1=int(input("enter a number n1: "))
+n2=int(input("enter a number n2: "))
+n3=int(input("enter a number n3: "))
+m=n1*n1+n2*n2
+n=n3*n3
+print(m)
+print(n)
+if m==n:
+    print("Pythogorean Triple or 1")
+else:
+    print("not a Pythogorean Triple or 0")
